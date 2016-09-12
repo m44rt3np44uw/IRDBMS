f8a67b6c41dae6d3a67f20a18cd2238deaf10432
Maarten Paauw <maartenpaauw@gmail.com>
2016-09-12 11:17:31 +0200

    Opdracht 3

---
+++ Week 2/opdracht_3.sql
@@ -0,0 +1,12 @@
+/**
+  Opdracht 3
+
+  Het aantal behandelingen per verrichting. Toon de verrichting
+  omschrijving.
+ */
+
+SELECT COUNT(behandeling.behandeling_nr), verrichting.verrichting_nr, verrichting.verrichting_omschrijving
+FROM behandeling
+JOIN verrichting
+ON behandeling.behandeling_verrichting_nr = verrichting.verrichting_nr
+GROUP BY verrichting.verrichting_nr, verrichting.verrichting_omschrijving
