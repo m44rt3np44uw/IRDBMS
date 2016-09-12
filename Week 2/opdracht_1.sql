96812723fa797f6ac647535b930d3b4918d0f56c
Maarten Paauw <maartenpaauw@gmail.com>
2016-09-12 11:07:16 +0200

    Opdracht 1

---
+++ Week 2/opdracht_1.sql
@@ -0,0 +1,12 @@
+/**
+  Opdracht 1
+
+  Een lijst op patient achternaam, tussenvoegsel en voornaam in welk bed
+  deze patient zich bevindt (bed nummer). Sorteer op achternaam.
+ */
+
+SELECT patient.patient_achternaam, patient.patient_tussenvoegsel, patient.patient_voornaam, bed.bed_nr
+FROM patient
+JOIN bed
+ON patient.patient_bed_nr = bed.bed_nr
+ORDER BY patient.patient_achternaam
